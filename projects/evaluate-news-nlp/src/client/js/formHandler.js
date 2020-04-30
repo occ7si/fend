545ab31c1a1d5af59af0addb13d1cdfa3f287604
caterineocampo <cocampo@gmx.de>
2020-04-30 14:31:50 +0200

    feat: Added fetch call to openweathermapapi

--- projects/evaluate-news-nlp/src/client/js/formHandler.js
+++ projects/evaluate-news-nlp/src/client/js/formHandler.js
@@ -1,3 +1,7 @@
+const API_KEY = '&appid=f12f38bf85fd07d6898ede03b27230fa';
+const BASE_URL = 'http://api.openweathermap.org/data/2.5/weather?zip=';
+const ZIP = '98001';
+
 export function handleSubmit(event) {
     event.preventDefault()
 
@@ -7,11 +11,12 @@ export function handleSubmit(event) {
 
     console.log("::: Form Submitted :::")
     // TODO: could go to an external API
-    fetch('http://localhost:3031/test')
+    // fetch('http://localhost:3031/test')
+    fetch(BASE_URL + ZIP + API_KEY)
     .then(res => res.json())
     .then(function(res) {
-        document.getElementById('results').innerHTML = res.message;
-        console.log('message is: ' + res.message);
+        document.getElementById('results').innerHTML = JSON.stringify(res);
+        console.log('message is: ' + JSON.stringify(res));
     })
 }
 
